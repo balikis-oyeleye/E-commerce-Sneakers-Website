643c1c652dfc57e626f0f3071d0553e3923541f3
Qreamville <Oyeleyebalikis@yahoo.com>
2022-05-09 00:35:32 -1200

    Update products.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Header from "./components/header/Header";
+import { Header, Products } from "./components";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Container>
+        <Products />
+      </Container>
     </div>
   );
 }
