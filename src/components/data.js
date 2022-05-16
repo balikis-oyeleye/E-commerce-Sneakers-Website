5a93de32e1aaf6bd80d8633b3e8eb5ccfb55a9b1
Qreamville <Oyeleyebalikis@yahoo.com>
2022-05-15 21:58:12 -1200

    Add files via upload

--- src/components/data.js
+++ src/components/data.js
@@ -1,141 +1,162 @@
+import image1 from "../assets/images/alexander-rotker-l8p1aWZqHvE-unsplash.jpg";
+import image2 from "../assets/images/daniel-storek-JM-qKEd1GMI-unsplash.jpg";
+import image3 from "../assets/images/imani-bahati-LxVxPA1LOVM-unsplash.jpg";
+import image4 from "../assets/images/joel-muniz-bS_1xFUPgWU-unsplash.jpg";
+import image5 from "../assets/images/kier-in-sight-R0mbwH2wxj0-unsplash.jpg";
+import image6 from "../assets/images/luis-felipe-lins-S6Cp3uN39_M-unsplash.jpg";
+import image7 from "../assets/images/mana-akbarzadegan-Y0RB2z12F1A-unsplash.jpg";
+import image8 from "../assets/images/ox-street-BWPqHZBhMVA-unsplash.jpg";
+import image9 from "../assets/images/photo-1549298916-f52d724204b4.jpeg";
+import image10 from "../assets/images/photo-1562273138-f46be4ebdf33.jpeg";
+import image11 from "../assets/images/photo-1574791418059-2e8d961ab4fb.jpeg";
+import image12 from "../assets/images/photo-1603487742131-4160ec999306.jpeg";
+import image13 from "../assets/images/photo-1604001307862-2d953b875079.jpeg";
+import image14 from "../assets/images/photo-1610398752800-146f269dfcc8.jpeg";
+import image15 from "../assets/images/photo-1618615098938-84fc29796e76.jpeg";
+import image16 from "../assets/images/photo-1625563206627-7e713d1ac0a8.jpeg";
+import image17 from "../assets/images/revolt-164_6wVEHfI-unsplash.jpg";
+import image18 from "../assets/images/ryan-plomp-76w_eDO1u1E-unsplash.jpg";
+import image19 from "../assets/images/ryan-plomp-jvoZ-Aux9aw-unsplash.jpg";
+import image20 from "../assets/images/the-dk-photography-b9e8ffVw5XY-unsplash.jpg";
+
 export const Data = [
   {
     id: 1,
     name: "Blue Air Max",
-    image: "./images/alexander-rotker-l8p1aWZqHvE-unsplash.jpg",
+    image: image1,
     price: "500",
     rating: 4,
   },
   {
     id: 2,
     name: "White Air Max",
-    image: "./images/daniel-storek-JM-qKEd1GMI-unsplash.jpg",
+    image: image2,
     price: "400",
     rating: 4,
   },
   {
     id: 3,
     name: "Breathable Unisex Footwear",
-    image: "./images/imani-bahati-LxVxPA1LOVM-unsplash.jpg",
+    image: image3,
     price: "450",
     rating: 3.5,
   },
   {
     id: 4,
     name: "Pink and Black",
-    image: "./images/joel-muniz-bS_1xFUPgWU-unsplash.jpg",
+    image: image4,
     price: "350",
     rating: 5,
   },
   {
     id: 5,
     name: "Cool Timberland",
-    image: "./images/kier-in-sight-R0mbwH2wxj0-unsplash.jpg",
+    image: image5,
     price: "1035",
     rating: 4,
   },
   {
     id: 6,
     name: "Polish Pink Nike",
-    image: "./images/luis-felipe-lins-S6Cp3uN39_M-unsplash.jpg",
+    image: image6,
     price: "740",
     rating: 4.5,
   },
   {
     id: 7,
     name: "Brown Tush Slide",
-    image: "./images/mana-akbarzadegan-Y0RB2z12F1A-unsplash.jpg",
+    image: image7,
     price: "550",
     rating: 3,
   },
   {
     id: 8,
     name: "Blue Panther Nike",
-    image: "./images/ox-street-BWPqHZBhMVA-unsplash.jpg",
+    image: image8,
     price: "300",
     rating: 4,
   },
   {
     id: 9,
     name: "Brown and White Sneakers",
-    image: "./images/photo-1549298916-f52d724204b4.jpeg",
+    image: image9,
     price: "690",
     rating: 4.5,
   },
   {
     id: 10,
     name: "Brown Sole Clogs",
-    image: "./images/photo-1562273138-f46be4ebdf33.jpeg",
+    image: image10,
     price: "560",
     rating: 5,
   },
   {
     id: 11,
     name: "Winged High Heels",
-    image: "./images/photo-1574791418059-2e8d961ab4fb.jpeg",
+    image: image11,
     price: "400",
     rating: 4.5,
   },
   {
     id: 12,
     name: "Grey Sandal",
-    image: "./images/photo-1603487742131-4160ec999306.jpeg",
+    image: image12,
     price: "670",
     rating: 4,
   },
   {
     id: 13,
     name: "3 inches Heels",
-    image: "./images/photo-1604001307862-2d953b875079.jpeg",
+    image: image13,
     price: "390",
     rating: 2.5,
   },
   {
     id: 14,
     name: "Heel Boot",
-    image: "./images/photo-1610398752800-146f269dfcc8.jpeg",
+    image: image14,
     price: "380",
     rating: 4,
   },
   {
     id: 15,
     name: "Beaded sandal",
-    image: "./images/photo-1618615098938-84fc29796e76.jpeg",
+    image: image15,
     price: "150",
     rating: 5,
   },
   {
     id: 16,
     name: "Cute sandal",
-    image: "./images/photo-1625563206627-7e713d1ac0a8.jpeg",
+    image: image16,
     price: "200",
     rating: 5,
   },
   {
     id: 17,
     name: "Red Nike Sneakers",
-    image: "./images/revolt-164_6wVEHfI-unsplash.jpg",
+    image: image17,
     price: "560",
     rating: 4.5,
   },
   {
     id: 18,
     name: "Pink Red Sneakers",
-    image: "./images/ryan-plomp-76w_eDO1u1E-unsplash.jpg",
+    image: image18,
     price: "340",
     rating: 3.5,
   },
   {
     id: 19,
     name: "Astronomical Sneakers",
-    image: "./images/ryan-plomp-jvoZ-Aux9aw-unsplash.jpg",
+    image: image19,
     price: "700",
     rating: 3.5,
   },
   {
     id: 20,
     name: "White Air Max",
-    image: "./images/the-dk-photography-b9e8ffVw5XY-unsplash.jpg",
+    image: image20,
     price: "300",
     rating: 4,
   },
